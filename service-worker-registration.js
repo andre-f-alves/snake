abe8a0bd0207c5735b1fd32c67a88ea88b6840d3
André Alves <andre.alves2006@hotmail.com>
2025-12-26 17:08:08 -0300

    Fixing PWA root directory

--- service-worker-registration.js
+++ service-worker-registration.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      await navigator.serviceWorker.register('service-worker.js')
+      await navigator.serviceWorker.register('./service-worker.js')
     
     } catch (error) {
       console.error(`Registration failed with ${error}`)
